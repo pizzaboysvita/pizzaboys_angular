4f328940e41296ea4f12af3498fa28cc14801018
sasidhar <sasidharsaikumarambati@gmail.com>
2025-04-26 19:13:11 +0530

    updated the ag agrid

--- src/app/components/restaurants/restaurants-list/restaurants-list.component.ts
+++ src/app/components/restaurants/restaurants-list/restaurants-list.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../../shared/components/card/card.component';
-import { TableComponent } from '../../widgets/table/table.component';
+// import { TableComponent } from '../../widgets/table/table.component';
 import { TableConfig } from '../../../shared/interface/table.interface';
 import { ProductsList } from '../../../shared/data/products';
-
+import { AgGridAngular } from '@ag-grid-community/angular'; 
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { ColDef, ModuleRegistry } from '@ag-grid-community/core';
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
+interface RowData {
+    make: string;
+    model: string;
+    price: number;
+  }
 @Component({
   selector: 'app-restaurants-list',
-  imports: [CardComponent,TableComponent],
+  imports: [CardComponent,AgGridAngular],
   templateUrl: './restaurants-list.component.html',
   styleUrl: './restaurants-list.component.scss'
 })
 export class RestaurantsListComponent {
+    modules = [ClientSideRowModelModule];
     public products = ProductsList;
     stausList=['Active','In-Active']
+    columnDefs: ColDef<RowData>[] = [    // <-- Important to give <RowData> here!
+        { field: 'make' },
+        { field: 'model' },
+        { field: 'price' }
+      ];
+    
+      rowData: RowData[] = [
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxster', price: 72000 }
+      ];
     public tableConfig: TableConfig = {
         columns: [
             // { title: "Product Image", dataField: 'product_image', type: 'image' },
