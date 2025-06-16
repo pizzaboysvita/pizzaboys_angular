66d8d90ed3df8054a911529ab2d972eeae25b8c4
sasidhar <sasidharsaikumarambati@gmail.com>
2025-06-16 22:00:33 +0530

    updated the reoved pages

--- src/app/components/support-ticket/support-ticket.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from "@angular/router";
-import { SupportTicketComponent } from "./support-ticket.component";
-
-export default [
-    {
-         path:'',
-         component:SupportTicketComponent
-    }
-] as Routes;
--- src/app/shared/routes/routes.ts
+++ src/app/shared/routes/routes.ts
@@ -17,10 +17,7 @@ export const content: Routes = [
         path: 'category',
         loadChildren: () => import('../../components/category/category.routes'),
     },
-    {
-        path: 'attributes',
-        loadChildren: () => import('../../components/attributes/attributes.routes'),
-    },
+   
     {
         path: 'restaurants',
         loadChildren: () => import('../../components/restaurants/restaurants.routes'),
@@ -29,10 +26,7 @@ export const content: Routes = [
         path: 'drivers',
         loadChildren: () => import('../../components/drivers/drivers.routes'),
     },
-    {
-        path: 'foods',
-        loadChildren: () => import('../../components/food/food.routes'),
-    },
+   
     {
         path: 'users',
         loadChildren: () => import('../../components/users/users.routes'),
@@ -45,34 +39,18 @@ export const content: Routes = [
         path: 'media',
         loadChildren: () => import('../../components/media/media.routes'),
     },
-    {
-        path: 'live-tracking',
-        loadChildren: () => import('../../components/live-tracking/live-tracking.routes'),
-    },
+   
     {
         path: 'orders',
         loadChildren: () => import('../../components/orders/orders.routes'),
     },
-    {
-        path: 'localization',
-        loadChildren: () => import('../../components/localization/localization.routes'),
-    },
-    {
-        path: 'coupons',
-        loadChildren: () => import('../../components/coupons/coupons.routes'),
-    },
-    {
-        path: 'tax',
-        loadChildren: () => import('../../components/tax/tax.routes'),
-    },
+   
+   
     {
         path: 'product-review',
         loadChildren: () => import('../../components/product-review/product-review.routes'),
     },
-    {
-        path: 'support-ticket',
-        loadChildren: () => import('../../components/support-ticket/support-ticket.routes'),
-    },
+
     {
         path: 'settings/profile-setting',
         loadChildren: () => import('../../components/setting/setting.routes'),
@@ -81,10 +59,7 @@ export const content: Routes = [
         path: 'reports',
         loadChildren: () => import('../../components/reports/reports.routes'),
     },
-    {
-        path: 'list-page',
-        loadChildren: () => import('../../components/list-page/list-page.routes'),
-    },
+   
     {
         path: 'staff',
         loadChildren: () => import('../../components/staff/staff.routes'),
